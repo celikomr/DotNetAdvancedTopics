4ff3f82c55e85ff2eafef109526c9d87b4725480
Ömer Çelik <oclk@outlook.com.tr>
2022-10-14 15:37:48 +0300

    refactor: Added comparison between Rest and gRPC to the GrpcServices README

--- GrpcServices/Services/GreeterService.cs
+++ GrpcServices/Services/GreeterService.cs
@@ -1,11 +1,11 @@
 using Grpc.Core;
-using GrpcServices;
 
 namespace GrpcServices.Services
 {
     public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
+
         public GreeterService(ILogger<GreeterService> logger)
         {
             _logger = logger;
