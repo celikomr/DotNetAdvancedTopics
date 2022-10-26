aa1f82be0a789481a9f75956aaa341b31ab38bc4
Ömer Çelik <oclk@outlook.com.tr>
2022-10-26 13:54:18 +0300

    refactor: Removed StreamingFromServer first func in SampleService

--- GrpcServices/Services/SampleService.cs
+++ GrpcServices/Services/SampleService.cs
@@ -10,15 +10,6 @@ public class SampleService : Sample.SampleBase
         return Task.FromResult(response);
     }
 
-    //public override async Task StreamingFromServer(SampleRequest request, IServerStreamWriter<SampleResponse> responseStream, ServerCallContext context)
-    //{
-    //    while (!context.CancellationToken.IsCancellationRequested)
-    //    {
-    //        await responseStream.WriteAsync(new SampleResponse());
-    //        await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
-    //    }
-    //}
-
     public override async Task StreamingFromServer(SampleRequest request, IServerStreamWriter<SampleResponse> responseStream, ServerCallContext context)
     {
         for (var i = 0; i < 5; i++)
