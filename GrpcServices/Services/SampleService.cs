a2186417d07576345b634af7afb64495ee67b679
Ömer Çelik <oclk@outlook.com.tr>
2022-10-26 10:25:34 +0300

    refactor: Removed user ops and added new sample models

---
+++ GrpcServices/Services/SampleService.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Core;
+
+namespace GrpcServices.Services;
+
+public class SampleService : Sample.SampleBase
+{
+    public override async Task StreamingFromServer(SampleRequest request, IServerStreamWriter<SampleResponse> responseStream, ServerCallContext context)
+    {
+        for (var i = 0; i < 5; i++)
+        {
+            await responseStream.WriteAsync(new SampleResponse());
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
