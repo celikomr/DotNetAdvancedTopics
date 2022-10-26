40ebd20c3e1a6834bfff0ee037905c9ddfe088e8
alperyalintatli <yalintatli23@gmail.com>
2022-10-26 16:45:48 +0300

    feat: Client StreamingFromClient method added

--- GrpcServices/Client/Program.cs
+++ GrpcServices/Client/Program.cs
@@ -9,13 +9,19 @@ var client = new Sample.SampleClient(channel);
 //Console.WriteLine("Response: " + reply.Message);
 
 
-var reply = client.StreamingFromServer(new SampleRequest { Message = "Sample Message", Index = 5 });
-while (await reply.ResponseStream.MoveNext())
+//var reply = client.StreamingFromServer(new SampleRequest { Message = "Sample Message", Index = 5 });
+//while (await reply.ResponseStream.MoveNext())
+//{
+//    Console.WriteLine("Response: " + reply.ResponseStream.Current);
+//}
+
+
+var reply = client.StreamingFromClient();
+for (int i = 0; i < 5; i++)
 {
-    Console.WriteLine("Response: " + reply.ResponseStream.Current);
+    await reply.RequestStream.WriteAsync(new SampleRequest() { Index = i });
 }
-
-
+await reply.RequestStream.CompleteAsync();
 
 Console.WriteLine("Shutting down");
 Console.WriteLine("Press any key to exit...");
