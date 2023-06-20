1e110da6d3dd624652aa84263024a9a0ce593607
Ömer Çelik <oclk@outlook.com.tr>
2023-06-20 16:09:25 +0300

    feat: Created MemoryCacheManager file and implement IMemoryCacheManager

--- InMemoryCaching/IMemoryCacheManager.cs
+++ InMemoryCaching/IMemoryCacheManager.cs
@@ -5,16 +5,14 @@ namespace InMemoryCaching;
 public interface IMemoryCacheManager : IDisposable
 {
     public void Clear();
-    public bool ContainsCacheKey(string key);
+    public bool ContainsKey(string key);
     public T Get<T>(string key);
     public IEnumerable<T> GetAll<T>();
-    public long GetCacheSize();
-    public long GetCacheSizeInBytes();
+    public T GetOrSet<T>(string key, Func<T> getDataFunc, TimeSpan expirationTime, IEnumerable<string> dependencies = null, bool useLazyLoading = true);
+    public long GetSize();
+    public long GetSizeInBytes();
     public void Remove(string key);
     public void Set<T>(string key, T value, TimeSpan expirationTime, IEnumerable<string> dependencies = null);
-    public void SetCacheCapacity(int size);
-    // public void RemoveCacheEntryRemovedCallback(CacheEntryRemovedCallback callback);
-    public void SetCacheOptions(string key, MemoryCacheEntryOptions options);
-
-    // public void RegisterCacheEntryRemovedCallback(CacheEntryRemovedCallback callback);
+    public void SetCapacity(int size);
+    public void SetOptions(string key, MemoryCacheEntryOptions options);
 }
