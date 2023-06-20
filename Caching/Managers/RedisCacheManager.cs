72642115f40e4a03f88b2b94df4e52bf16ff3d3a
Ömer Çelik <oclk@outlook.com.tr>
2023-06-21 01:58:59 +0300

    feat: Added RedisCacheManager

---
+++ Caching/Managers/RedisCacheManager.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Caching.Managers;
+
+public class RedisCacheManager : ICacheManager
+{
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ContainsKey(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Get<T>(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> GetAll<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T GetOrSet<T>(string key, Func<T> getDataFunc, TimeSpan expirationTime, IEnumerable<string> dependencies = null, bool useLazyLoading = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Set<T>(string key, T value, TimeSpan expirationTime, IEnumerable<string> dependencies = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetCapacity(int size)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetOptions(string key, MemoryCacheEntryOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
