6af1aa4667df1a8302ce848d5decad83a58c955e
Ömer Çelik <oclk@outlook.com.tr>
2023-06-21 01:57:08 +0300

    feat: Created MemoryCacheManager

--- Caching/Managers/ICacheManager.cs
+++ Caching/Managers/ICacheManager.cs
@@ -2,15 +2,13 @@
 
 namespace Caching.Managers;
 
-public interface ICacheManager
+public interface ICacheManager : IDisposable
 {
     public void Clear();
     public bool ContainsKey(string key);
     public T Get<T>(string key);
     public IEnumerable<T> GetAll<T>();
     public T GetOrSet<T>(string key, Func<T> getDataFunc, TimeSpan expirationTime, IEnumerable<string> dependencies = null, bool useLazyLoading = true);
-    public long GetSize();
-    public long GetSizeInBytes();
     public void Remove(string key);
     public void Set<T>(string key, T value, TimeSpan expirationTime, IEnumerable<string> dependencies = null);
     public void SetCapacity(int size);
